9ea7d79eee5437d3009991cf790debd526505f27
Vladimir Levadny <v.levadny@rambler.ru>
2021-12-04 03:47:09 +0300

    The initial layout is done

---
+++ src/js/filterPortfolio.js
@@ -0,0 +1,133 @@
+// Блок констант:
+// - menu - обвертка для блока кнопок;
+// - menuItems - все кнопки;
+// - btnAll, btnHtml, btnJs, btnReact - кнопки;
+// - items - все блоки с фотографиями,
+// - itemAll, itemHtml, itemJs, itemReact - блоки по классам
+
+const menu = document.querySelector('.portfolio__menu'),
+	  menuItems = menu.querySelectorAll('li'),
+	  btnAll = menu.querySelector('.all'),
+	  btnHtml = menu.querySelector('.html'),
+	  btnJs = menu.querySelector('.js'),
+	  btnReact = menu.querySelector('.react'),
+	  items = document.querySelector('.portfolio__items'),
+	  itemAll = items.querySelectorAll('.all'),
+	  itemHtml = items.querySelectorAll('.html_css'),
+	  itemJs = items.querySelectorAll('.js'),
+	  itemReact = items.querySelectorAll('.react');
+
+// Константа, задающая значение свойства display для элементов
+const displayStatus = 'list-item';
+// Константа задающая паузу при анимации в миллисекундах
+const animationPause = 700;
+
+
+// Функция сортировки эллементов при клике
+const sortItems = (eventClass) => {
+	// Массивы, в которые будут записаны элементы для скрытия (itemsHide)
+	// и отображения (itemsAppear)
+	let itemsAppear = [],
+		itemsHide = [];
+
+	itemAll.forEach(item => {
+		if (item.classList.contains(eventClass)) {
+			if (item.style.display != displayStatus) {
+				itemsAppear.push(item);
+			}
+		} else {
+			itemsHide.push(item);
+		}
+
+	})
+
+	if (itemsHide.length > 0) {
+		toHideItems(itemsHide);
+		setTimeout(() => toAppearItems(itemsAppear), animationPause);
+	} else {
+		toAppearItems(itemsAppear);
+	}
+	
+	
+}
+
+
+// Функция вызывающая всплытие элементов
+const toAppearItems = (arr) => {
+	arr.forEach((item, index) => {
+		item.style.display = displayStatus;
+		item.querySelector('.info').style.display = 'none';
+		item.classList.remove('hide-left', 'hide-right');
+	
+		if ((index + 1) % 2 == 0) {
+			item.classList.add("appear-left")
+		} else {
+			item.classList.add("appear-right")
+		}
+	});
+}
+
+// Функция вызывающая скрытие элементов
+const toHideItems = (arr) => {
+	arr.forEach((item, index) => {
+		item.querySelector('.info').style.display = 'none';
+		item.classList.remove('appear-left', 'appear-right')
+	
+		if ((index + 1) % 2 == 0) {
+			item.classList.add("hide-left");
+			setTimeout(() => {item.style.display = 'none'}, animationPause)
+		} else {
+			item.classList.add("hide-right")
+			setTimeout(() => {item.style.display = 'none'}, animationPause)
+		}
+	});
+}
+
+
+
+
+const typeFilter = (event) => {
+	const target = event.target,
+		  btnEvent = target.closest('li');
+
+	if (!target || !(target.tagName == "DIV" || target.tagName == "LI")) {
+		return;
+	}
+
+	// Меняем класс активности у кнопок при нажатии
+	menuItems.forEach(item => {
+		if (item != btnEvent) {
+			item.classList.remove('active')
+		} else {
+			item.classList.add('active')
+		}
+	});
+
+	// Вызов функции для сортировки эллементов при нажатии на кнопку
+	switch (btnEvent) {
+		case btnAll:
+			sortItems('all');
+			break;
+		case btnHtml:
+			sortItems('html_css');
+			break;
+		case btnJs:
+			sortItems('js');
+			break;
+		case btnReact:
+			sortItems('react');
+			break;
+		default:
+			console.log("Что-то пошло не так");
+			break;
+	}
+
+
+
+};
+
+// первоначальная загрузка элементов
+toAppearItems(itemAll);
+
+// Обработчик событий для всех кнопок
+menu.addEventListener('click', e => typeFilter(e));
---
+++ src/js/portfolioAnimation.js
@@ -0,0 +1,53 @@
+// Блок констант:
+// wrapper - родительский элемент, на который вешается обработчики для списка
+// directions, prefixDirection - составные имени класса для анимирования
+// classNames - список всех имен классов для очистки 
+const wrapper = document.querySelector(".portfolio__items"),
+	  directions = ["top", "right", "bottom", "left"],
+	  prefixDirection = ["in", "out"],
+	  classNames = prefixDirection
+	  	.map(pr => directions.map(dir => `${pr}-${dir}`)).flat();
+		  
+// Функция, определяющая направление события в элементе
+const getDirectionMove = (target, event) => {
+	const { width, height, top, left } = target.getBoundingClientRect();
+
+	const leftPosition = event.pageX - (left + window.scrollX),
+		  topPosition = event.pageY - (top + window.scrollY),
+		  x = leftPosition - (width / 2) * (width > height ? height / width : 1),
+		  y = topPosition - (height / 2) * (height > width ? width / height : 1);
+
+	return Math.round(Math.atan2(y, x) / 1.57079633 + 5) % 4;
+}
+
+
+// Функция обновляющая состояние элемента события
+const updateState = (event, prefix) => {
+	const target = event.target;
+
+	if (target && target.tagName === 'IMG') {
+		const  className = `${prefix}-${directions[getDirectionMove(target, event)]}`;
+		const parentItem = target.closest(".portfolio__item")
+		
+		parentItem.classList.remove(...classNames);
+		parentItem.classList.add(className);
+
+		parentItem.querySelector('.info').style.display = 'block';
+		
+		// Задаем перспективу для элемента
+		const { width, height } = target.getBoundingClientRect();
+		const elementIndex = classNames.findIndex(elem => elem == className);
+		if (elementIndex % 2 !== 0) {
+			target.closest(".portfolio__item").style.perspective = `${width * 2}px`;
+		} else {
+			target.closest(".portfolio__item").style.perspective = `${height * 2}px`;
+		}
+	}
+}
+
+wrapper.addEventListener("mouseover", e => {
+	updateState(e, 'in');
+});
+wrapper.addEventListener("mouseout", e => {
+	updateState(e, 'out')
+});
